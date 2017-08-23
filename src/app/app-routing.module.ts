9a828937083035f27cf345d528466279d6a639b7
Mickael <mickael.rolo@gmail.com>
2017-08-23 16:18:09 +0200

    Portal by default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule }             from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', loadChildren: 'app/portal/portal.module#PortalModule' },
   { path: 'portal', loadChildren: 'app/portal/portal.module#PortalModule' }
 ];
 
